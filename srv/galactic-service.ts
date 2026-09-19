89987d1245bc6aa0d45e302db3adf4ca39c89803
Krisztian Nyikos <krisz.nyikos@gmail.com>
2026-09-19 08:33:59 +0200

    refactor: streamline validation logic for SpaceFarers creation

--- srv/galactic-service.ts
+++ srv/galactic-service.ts
@@ -5,23 +5,21 @@ const LOG = cds.log("galactic-service");
 export default class GalacticService extends cds.ApplicationService {
   async init() {
     this.before("CREATE", "SpaceFarers", (req) => {
-      const { stardustCollection, wormholeNavigation } = req.data;
-
-      if (stardustCollection === undefined) {
-        req.data.stardustCollection = 0;
-      }
-
-      if (wormholeNavigation === undefined) {
+      if (req.data.wormholeNavigation === undefined) {
         req.error(400, "wormholeNavigation is required");
       }
 
-      if (stardustCollection !== undefined && stardustCollection < 0) {
+      if (req.data.stardustCollection !== undefined && req.data.stardustCollection < 0) {
         req.error(400, "stardustCollection must be non-negative");
       }
 
+      if (req.data.stardustCollection === undefined) {
+        req.data.stardustCollection = 0;
+      }
+
       if (
-        wormholeNavigation !== undefined &&
-        (wormholeNavigation < 0 || wormholeNavigation > 10)
+        req.data.wormholeNavigation !== undefined &&
+        (req.data.wormholeNavigation < 0 || req.data.wormholeNavigation > 10)
       ) {
         req.error(400, "wormholeNavigation must be between 0 and 10");
       }
